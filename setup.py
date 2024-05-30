c6771fad26399d84d7cab3802ffa502e38847cde
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-05-30 16:55:50 +0530

    Added folder Structure

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'  # this things has been defined in the requirement.txt file, the use of this is to trigger the setup.py file when it will reun requirements.txt file.
+HYPEN_E_DOT = '-e .'  # this things has been defined in the requirement.txt file, the use of this is to trigger the setup.py file when it will reun requirements.txt file.
 
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
