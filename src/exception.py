da3b49e60fa3733f6f876eba6f65c49b55341127
rakeshAtWork <workrakros252525@gmail.com>
2024-07-28 15:03:43 +0530

    Many Chnages

--- src/exception.py
+++ src/exception.py
@@ -22,13 +22,12 @@ class CustomException(Exception):
         return self.error_message    
     
 # our aim is to run the current main file.
-'''
-if __name__=="__main__":
-    logging.info("Logging has started")
 
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Dicision by zero') 
-        raise CustomException(e,sys)
-'''
+# if __name__=="__main__":
+#     logging.info("Logging has started")
+
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info('Dicision by zero') 
+#         raise CustomException(e,sys)
